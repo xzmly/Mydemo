b125c43e0383a590703c5c95aeadbd68440d7b55
lucien <30624842+xzmly@users.noreply.github.com>
2017-10-14 21:11:23 +0800

    Update router.js

--- small demo/加载数据/router.js
+++ small demo/加载数据/router.js
@@ -1,4 +1,4 @@
- app.get('https://xzmly.github.io/loadMore?index=6&length=5',function(req,res){
+ app.get('http://loadMore',function(req,res){
 
  	var curIdx = req.query.index
  	var len = req.query.length
